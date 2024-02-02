839e81ff0f58327435b584fc4400e4c280397597
Guillaume MODARD <guillaumemodard@gmail.com>
2024-02-02 16:02:53 +0100

    Build

--- lib/cjs/classes/Request.d.ts
+++ lib/cjs/classes/Request.d.ts
@@ -1,4 +1,15 @@
-import { RequestOptionsInterface } from "../interfaces/RequestOptionsInterface";
 export declare class Request {
-    constructor(url: string, params?: object, options?: RequestOptionsInterface);
+    readonly url: string;
+    readonly params?: object;
+    readonly options?: RequestInit;
+    private response?;
+    constructor(url: string, params?: object, options?: RequestInit);
+    /**
+     * Send the request and store the response in the Request object
+     */
+    send(): Promise<this>;
+    /**
+     * Get the response from the request object
+     */
+    getResponse(): Response | undefined;
 }
--- lib/cjs/services/request.d.ts
+++ lib/cjs/services/request.d.ts
@@ -1,3 +1,21 @@
 import { Request } from "../classes/Request";
-export declare function getRequest(url: string, params?: object): Request;
-export declare function postRequest(url: string, params?: object): Request;
+/**
+ * Create a get request.
+ *
+ * If params are provided, they will be concat onto the url. For GET request, params must respect URL limitations.
+ *
+ * @param url               URL onto which the request will be sent
+ * @param params            URL parameters
+ * @param options           Fetch options that will be sent with the request
+ */
+export declare function getRequest(url: string, params?: Record<string, any>, options?: RequestInit): Request;
+/**
+ * Create a post request
+ *
+ * By default, params will be sent with application/json encoding through the body options.
+ *
+ * @param url               URL onto which the request will be sent
+ * @param params            Request parameters
+ * @param options           Fetch options that will be sent with the request
+ */
+export declare function postRequest(url: string, params?: object, options?: RequestInit): Request;
